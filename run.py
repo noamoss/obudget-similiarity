001e3722352f29ce07e5877fadaf85d0bef40c8d
Noam <noamoss@gmail.com>
2018-06-01 11:57:52 +0300

    fix typo on run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from data_loader import load_datapackage
 from tokenize_df import tokenize_row
 
 
-print("Starting to load data, enetity to explore: {} Based on sources file named {}".fromat(ENTITY_TO_EXPLORE, SOURCES_FILE))
+print("Starting to load data, enetity to explore: {} Based on sources file named {}".format(ENTITY_TO_EXPLORE, SOURCES_FILE))
 df = load_datapackage(ENTITY_TO_EXPLORE)
 print("Starting to tokenize to tokenize dataframe's items")
 df["tokenized"] = df.apply(tokenize_row, axis=1)
